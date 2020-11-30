af549c9d157b97946200f70438a2cbe41454754c
porkostomus <porkostomus@mail.com>
2020-11-30 02:26:19 -0800

    add computerphile graph

--- src/porkostomus/dijkstra.clj
+++ src/porkostomus/dijkstra.clj
@@ -147,7 +147,26 @@ cost
   (dijkstra :red :green demo-graph)
   (dijkstra-pm :red :green demo-graph))
 
+(def computerphile
+  {:s {:a 7 :b 2 :c 3}
+   :a {:s 7 :b 3 :d 4}
+   :b {:s 2 :a 3 :d 4 :h 1}
+   :c {:s 3 :l 2}
+   :d {:a 4 :b 4 :f 5}
+   :e {:g 2 :k 5}
+   :f {:d 5 :h 3}
+   :g {:h 2 :e 2}
+   :h {:b 1 :f 3 :g 2}
+   :i {:l 4 :j 6 :k 4}
+   :j {:i 6 :l 4 :k 4}
+   :k {:i 4 :j 4 :e 5}})
+
+(dijkstra :s :e computerphile)
+
 ; 1. Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
+; 2. Assign to every node a tentative distance value: 
+;    set it to zero for our initial node and to infinity for all other nodes.
+;    Set the initial node as current.
 
 (def graph-db
   (atom {:nodes {}
@@ -164,8 +183,18 @@ cost
 
 @graph-db
 
-; 2. Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all other nodes. Set the initial node as current.[16]
-; 3. For the current node, consider all of its unvisited neighbours and calculate their tentative distances through the current node. Compare the newly calculated tentative distance to the current assigned value and assign the smaller one. For example, if the current node A is marked with a distance of 6, and the edge connecting it with a neighbour B has length 2, then the distance to B through A will be 6 + 2 = 8. If B was previously marked with a distance greater than 8 then change it to 8. Otherwise, the current value will be kept.
+; 3. For the current node, consider all of its unvisited neighbours
+;    and calculate their tentative distances through the current node.
+;    Compare the newly calculated tentative distance to the current assigned value and assign the smaller one.
+
+;    For example, if the current node A is marked with a distance of 6,
+;    and the edge connecting it with a neighbour B has length 2,
+;    then the distance to B through A will be 6 + 2 = 8.
+;    If B was previously marked with a distance greater than 8 then change it to 8.
+;    Otherwise, the current value will be kept.
+
+
+
 ; 4. When we are done considering all of the unvisited neighbours of the current node, mark the current node as visited and remove it from the unvisited set. A visited node will never be checked again.
 ; 5. If the destination node has been marked visited (when planning a route between two specific nodes) or if the smallest tentative distance among the nodes in the unvisited set is infinity (when planning a complete traversal; occurs when there is no connection between the initial node and remaining unvisited nodes), then stop. The algorithm has finished.
 ; 6. Otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new "current node", and go back to step 3.
