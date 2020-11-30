be3bc854c503ff76d8b1429683e3bebc897c8181
porkostomus <porkostomus@mail.com>
2020-11-29 22:22:56 -0800

    use unvisited set

--- src/porkostomus/dijkstra.clj
+++ src/porkostomus/dijkstra.clj
@@ -147,19 +147,25 @@ cost
   (dijkstra :red :green demo-graph)
   (dijkstra-pm :red :green demo-graph))
 
+; 1. Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
+
 (def graph-db
-  (atom {:nodes (zipmap (keys demo-graph)
-                        (repeat {:visited? false
-                                 :distance Integer/MAX_VALUE}))
+  (atom {:nodes {}
+         :unvisited #{}
          :current-node nil}))
 
-(defn initial-distances [initial-node]
-  (swap! graph-db assoc-in
-         [:nodes initial-node] {:distance 0
-                                :current? true
-                                :visited? false})
+(defn initial-distances [graph initial-node]
+  (swap! graph-db assoc :nodes (zipmap (keys graph) (repeat Integer/MAX_VALUE)))
+  (swap! graph-db assoc :unvisited (set (keys graph)))
+  (swap! graph-db assoc-in [:nodes initial-node] 0)
   (swap! graph-db assoc :current-node initial-node))
 
-(initial-distances :red)
+(initial-distances demo-graph :red)
 
 @graph-db
+
+; 2. Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all other nodes. Set the initial node as current.[16]
+; 3. For the current node, consider all of its unvisited neighbours and calculate their tentative distances through the current node. Compare the newly calculated tentative distance to the current assigned value and assign the smaller one. For example, if the current node A is marked with a distance of 6, and the edge connecting it with a neighbour B has length 2, then the distance to B through A will be 6 + 2 = 8. If B was previously marked with a distance greater than 8 then change it to 8. Otherwise, the current value will be kept.
+; 4. When we are done considering all of the unvisited neighbours of the current node, mark the current node as visited and remove it from the unvisited set. A visited node will never be checked again.
+; 5. If the destination node has been marked visited (when planning a route between two specific nodes) or if the smallest tentative distance among the nodes in the unvisited set is infinity (when planning a complete traversal; occurs when there is no connection between the initial node and remaining unvisited nodes), then stop. The algorithm has finished.
+; 6. Otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new "current node", and go back to step 3.
